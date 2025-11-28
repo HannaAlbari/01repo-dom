e82ed209e760169c5c4b6915d6ba345ead311200
HannaAlbari <halbari@st.swps.edu.pl>
2025-11-28 13:29:35 +0100

    views.py.zmiana

--- system rezerwacyjny/rezerwacja samochodow/views.py
+++ system rezerwacyjny/rezerwacja samochodow/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Mój widok
+from rest_framework.views import APIView
+#szybkie tworzenie kompletnych CRUD
+from rest_framework import viewsets, generics
+#zwracanie danych JSON
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+
+from .models import *
+from .serializers import *
+from .permissions import IsAdminOrReadOnly
