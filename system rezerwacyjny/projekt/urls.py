caa00ba069ac69dc8e3e54188de5a926cf19b34c
HannaAlbari <halbari@st.swps.edu.pl>
2026-01-15 17:19:32 +0100

    urls-admin

--- system rezerwacyjny/projekt/urls.py
+++ system rezerwacyjny/projekt/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('rezerwacja samochodow/', include('rezerwacja samochodow.urls')),
     path('admin/', admin.site.urls),
 ]
 
