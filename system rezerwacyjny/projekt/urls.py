09faea5c99978b6570d1b8e8ce0175ed732a5e81
HannaAlbari <halbari@st.swps.edu.pl>
2026-01-23 13:32:31 +0100

    dodano base.html, home.html

--- system rezerwacyjny/projekt/urls.py
+++ system rezerwacyjny/projekt/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('rezerwacja_samochodow/', include('rezerwacja_samochodow.urls')),
     path('admin/', admin.site.urls),
